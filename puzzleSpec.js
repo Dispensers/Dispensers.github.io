997207c3f1406973ba6f9d0dcf1e377423aab964
Dispensers <121032668+Dispensers@users.noreply.github.com>
2023-10-14 23:02:17 +0100

    Add files via upload

---
+++ puzzleSpec.js
@@ -0,0 +1,9 @@
+let punterPuzzleSpec = {
+	number: 19,
+	solveBy: "17 Nov",
+	dispenserSpec: [undefined, "-1", "4*k", "71"],
+	targetSpec: "147",
+	hintSpec: {numDots: 2, symbol:"3", isHere: false},
+	solutionExpression: "11*14-7",
+	solutionDispenseSequence: [1, 2, 2, 3, 2, 1, 3]
+};
