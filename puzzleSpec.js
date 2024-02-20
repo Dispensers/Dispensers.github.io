fc1cb28dadcca97e1090b395853b53c9d113e2a8
Dispensers <121032668+Dispensers@users.noreply.github.com>
2024-02-20 20:21:58 +0000

    Add files via upload

--- puzzleSpec.js
+++ puzzleSpec.js
@@ -1,15 +1,103 @@
+/*
 let punterPuzzleSpec = {
-	number: 151,
-	solveBy: "31 Dec",
-	words: ["SKINT", "SLANT", "SLAIN", "AGAIN"],
-	hintSpec: [2, 3, "I"],
-	featuredWordBlurb:
-		"Just not getting lost required all Santa's skill ...<br>\
-		With street signs and numbers more difficult still.<br>\
-		He tangled in tree-tops <strong>AGAIN</strong> and <strong>AGAIN</strong>,<br>\
-		And barely missed hitting a tri-motored plane.<br>\
-		<br>\
-		From a draft of 'Rudolph, The Red-Nosed Reindeer' by Robert L. May, 1939."
+	number: 12,
+	solveBy: "6 Feb",
+	dispenserSpec: [undefined, "26", "4*", "/05"],
+	targetSpec: "200",
+	hintSpec: {numDots: 3, symbol:"0", isHere: false},
+	solutionExpression: "50/6*24",
+	solutionDispenseSequence: [3, 3, 3, 1, 2, 1, 2]
+};
+*/
+/*
+let punterPuzzleSpec = {
+	number: 15,
+	solveBy: "13 Feb",
+	dispenserSpec: [undefined, "-1", "4*1", "71"],
+	targetSpec: "147",
+	hintSpec: {numDots: 3, symbol:"7", isHere: false},
+	solutionExpression: "11*14-7",
+	solutionDispenseSequence: [1, 2, 2, 3, 2, 1, 3]
+};
+*/
+
+let punterPuzzleSpec = {
+	number: 13,
+	solveBy: "20 Feb",
+	dispenserSpec: [undefined, "333", "+", "-", "21"],
+	targetSpec: "-5",
+	hintSpec: {numDots: 1, symbol:"3", isHere: false},
+	solutionExpression: "3-31+23",
+	solutionDispenseSequence: [1, 3, 1, 4, 2, 4, 1]
 };
 
+/*
+let punterPuzzleSpec = {
+	number: 23,
+	solveBy: "27 Feb",
+	dispenserSpec: [undefined, "13", "5", "79", "-", "*"],
+	targetSpec: "318",
+	hintSpec: {numDots: 1, symbol:"9", isHere: false},
+	solutionExpression: "9*37-15",
+	solutionDispenseSequence: [3, 5, 1, 3, 4, 1, 2]
+};
+*/
+/*
+let punterPuzzleSpec = {
+	number: 17,
+	solveBy: "5 Mar",
+	dispenserSpec: [undefined, "20", "/", "+8", "46"],
+	targetSpec: "4.15",
+	hintSpec: {numDots: 1, symbol:"9", isHere: false},
+	solutionExpression: "86/40+2",
+	solutionDispenseSequence: [3, 4, 2, 4, 1, 3, 1]
+};
+*/
+/*
+let punterPuzzleSpec = {
+	number: 14,
+	solveBy: "12 Mar",
+	dispenserSpec: [undefined, "/61", "7", "91-8"],
+	targetSpec: "53",
+	hintSpec: {numDots: 1, symbol:"9", isHere: false},
+	solutionExpression: "81-196/7",
+	solutionDispenseSequence: [3, 1, 3, 3, 3, 1, 1, 2]
+};
+*/
+/*
+let punterPuzzleSpec = {
+	number: 22,
+	solveBy: "19 Mar",
+	dispenserSpec: [undefined, "1", "2+", "3", "4-", "5"],
+	targetSpec: "-28",
+	hintSpec: {numDots: 1, symbol:"9", isHere: false},
+	solutionExpression: "1-53+24",
+	solutionDispenseSequence: [1, 4, 5, 3, 2, 2, 4]
+};
+*/
+/*
+let punterPuzzleSpec = {
+	number: 16,
+	solveBy: "?? Apr",
+	dispenserSpec: [undefined, "5-1", "159", "-19"],
+	targetSpec: "-37",
+	hintSpec: {numDots: 1, symbol:"9", isHere: false},
+	solutionExpression: "915-951-1",
+	solutionDispenseSequence: [2, 1, 2, 1, 3, 1, 3, 3, 2]
+};
+*/
 
+/* ================================================================= */
+
+
+
+/*
+let punterPuzzleSpec = {
+	number: 99,
+	solveBy: "30 Jan",
+	dispenserSpec: [undefined, "/9", "6-", "38", "0", "1"],
+	targetSpec: "28",
+	solutionExpression: "90-186/3",
+	solutionDispenseSequence: [1, 4, 2, 5, 3, 2, 1, 3]
+};
+*/
